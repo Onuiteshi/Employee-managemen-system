aa905948c3b6129cc7d8a21e0d2f98c6e20cfde5
Emmanuel Onuiteshi <emmanuel.onuiteshi@doftwerks.com>
2022-04-26 22:32:08 +0100

    feat: added login page

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,82 @@
+import { TextField } from "@mui/material";
+import { Container } from "@mui/material";
+import { Box } from "@mui/material";
+import { Grid } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { Button } from "@mui/material";
+import "./login.css";
+import React from "react";
+import { useForm } from "react-hook-form";
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="loginContainer">
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Container maxWidth="xs">
+          <Card variant="outlined">
+            <CardContent>
+              <div className="logo">
+                <h1>HRM</h1>
+              </div>
+              <form onSubmit={handleSubmit(submitForm)}>
+                <Box mb={1}>
+                  <TextField
+                    variant="outlined"
+                    label="email"
+                    fullWidth
+                    autoFocus
+                    size="small"
+                    {...register("email", {
+                      required: "Email is Required",
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "Invalid Email Address",
+                      },
+                    })}
+                    error={!!errors?.email}
+                    helperText={errors?.email ? errors.email.message : null}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    autoFocus
+                    size="small"
+                    margin="normal"
+                    {...register("password", {
+                      required: "Password is Required",
+                    })}
+                    error={!!errors?.password}
+                    helperText={
+                      errors?.password ? errors.password.message : null
+                    }
+                  />
+                </Box>
+                <Button type="submit" variant="contained" fullWidth>
+                  Login
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </Container>
+      </Grid>
+    </div>
+  );
+};
+
+export default Login;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "../pages/Login";
+import Login from "../pages/Login/Login";
 import Layout from "../layout";
 import Dashboard from "../pages/Dashboard";
+import Employees from "../pages/Employees";
 
 const index = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Layout />}>
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="employees" element={<Employees />} />
         </Route>
       </Routes>
     </BrowserRouter>
